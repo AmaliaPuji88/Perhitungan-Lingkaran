cb68c83788d80d04300fdfb4650857c12a4c9dfb
AmaliaPuji88 <154043597+AmaliaPuji88@users.noreply.github.com>
2023-12-17 13:47:24 +0700

    Add files via upload

---
+++ Perhitungan-Lingkaran/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Program Menghitung Luas Lingkaran");
+        Console.WriteLine(new string('-', 35));
+
+        // Meminta pengguna memasukkan jari-jari lingkaran dalam sentimeter
+        Console.Write("   Masukkan panjang jari-jari dalam sentimeter: ");
+        double jariJariCm = Convert.ToDouble(Console.ReadLine());
+
+        // Menghitung luas lingkaran dalam sentimeter
+        double luasLingkaranCm = HitungLuasLingkaran(jariJariCm);
+
+        // Menampilkan hasil
+        Console.WriteLine("\n   Luas Lingkaran dengan jari-jari {0} cm adalah {1:F2} cm^2", jariJariCm, luasLingkaranCm);
+    }
+
+    // Fungsi untuk menghitung luas lingkaran dalam sentimeter
+    static double HitungLuasLingkaran(double jariJariCm)
+    {
+        // Rumus luas lingkaran: π * r^2
+        double luas = Math.PI * Math.Pow(jariJariCm, 2);
+        return luas;
+    }
+}
